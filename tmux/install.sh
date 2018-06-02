d9675dba2d563e455782de91c3f74bce22f702e6
ssfdust <ssfdust@gmail.com>
2018-06-02 18:43:11 +0800

    uodate

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,3 +5,5 @@ echo "$BASEDIR"
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf "$BASEDIR/tmux.conf" "$HOME/.tmux.conf"
+sed 's/⎆/爵/g' -i ~/.tmux/plugins/tmux-powerline/segments/ifstat_sys.sh
+echo cp "$BASEDIR/ssfdust.sh" "~/.tmux/plugins/tmux-powerline/themes/"

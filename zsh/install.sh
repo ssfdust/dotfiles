0b8c92997af957704894c10f29921142f8b14215
ssfdust <ssfdust@gmail.com>
2018-10-07 21:52:42 +0800

    update

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ ! -d "$HOME/.oh-my-zsh" ]];then
+    echo "You need to install oh-my-zsh first, and then run this script"
+    echo "running the following command"
+    echo 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+    exit
+fi
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions

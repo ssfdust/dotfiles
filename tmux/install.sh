4acb9bde3e384c66c6e6f60a58b17feca92677d1
ssfdust <ssfdust@gmail.com>
2018-10-07 21:52:50 +0800

    update

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,5 +5,10 @@ echo "$BASEDIR"
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf "$BASEDIR/tmux.conf" "$HOME/.tmux.conf"
-sed 's/⎆/爵/g' -i ~/.tmux/plugins/tmux-powerline/segments/ifstat_sys.sh
-echo cp "$BASEDIR/ssfdust.sh" "~/.tmux/plugins/tmux-powerline/themes/"
+if [[ -d "$HOME/.tmux/plugins/tmux-powerline" ]];then
+    sed 's/⎆/爵/g' -i ~/.tmux/plugins/tmux-powerline/segments/ifstat_sys.sh
+    echo cp "$BASEDIR/ssfdust.sh" "~/.tmux/plugins/tmux-powerline/themes/"
+else
+    echo "please enter the tmux mode and install the plugins via C-A + I(大写的I, Capital I)"
+    echo "then run the script again"
+fi

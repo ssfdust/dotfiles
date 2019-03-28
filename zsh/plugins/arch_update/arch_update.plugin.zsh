8827c14f4b04ab6a9da44779d0ce1ce1310b0643
ssfdust <ssfdust@gmail.com>
2019-03-28 21:55:46 +0800

    update arch_update script

--- zsh/plugins/arch_update/arch_update.plugin.zsh
+++ zsh/plugins/arch_update/arch_update.plugin.zsh
@@ -10,6 +10,19 @@ bb_ins() {
     sudo bb-wrapper -S $@ --aur --build-dir ~/Update_pkg
 }
 
+bb_clean() {
+    echo "Do you want to clean Update_pkg directoy?[y/N]"
+    read answer
+    if [[ $answer =~ "[Yy]" && -d ~/Update_pkg ]]; then 
+        for dir in ~/Update_pkg/*/; do
+            pushd $dir
+            git clean -xfd
+            popd
+        done
+    fi
+}
+
 bb_update() {
+    bb_clean
     sudo bb-wrapper -Syyu --aur --build-dir ~/Update_pkg
 }

fd3306e4da25780d529e0988d9503c8dad7be825
ssfdust <ssfdust@gmail.com>
2020-11-26 09:30:41 +0800

    update

--- firefox/install.py
+++ firefox/install.py
@@ -1,57 +1,105 @@
 #!/bin/env python3
+"""读取并处理firefox的profiles.ini的文件
+
+每个profiles都有N个ProflieX构成和一个Installxxx构成
+
+例如：
+[Install4F96D1932A9F858E]
+Default=fc8ptlr3.default-release
+Locked=1
+
+[Profile1]
+Name=default
+IsRelative=1
+Path=4z3fjqne.default
+Default=1
+
+[Profile0]
+Name=default-release
+IsRelative=1
+Path=fc8ptlr3.default-release
+
+首先判断是否存在Default并且这个Default的值是一个Path，
+同时这个Default所在这个Section不是以Profile开头，然后就能
+拿到Default Profile的Path
+"""
 from pathlib import Path
 import configparser
 import logging
+import shutil
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.warning("开始检查")
 
+COPY_TARGETS = ["chrome", "user.js"]
+src_path = Path(__file__).absolute().parent
+
+
 def parserini(configpath):
     """处理配置文件"""
     config = configparser.ConfigParser()
     config.read(configpath)
-    for path in parsepath(config):
-        for default in parsedefault(config):
+    directory = configpath.parent
+    for path in parsepath(config, directory):
+        for default in parsedefault(config, directory):
+            defaultpath = Path(directory, default)
             if path == default:
-                chromepath = configpath.parent.joinpath(path,
-                                                        'chrome')
-                if not chromepath.exists():
-                    chromepath.mkdir()
-                return chromepath
+                return defaultpath
 
-def parsepath(config):
-    for nsp, section in config.items():
-        if 'Path' in section:
-            yield section['Path']
 
-def parsedefault(config):
+def parsepath(config, directory):
+    """从每一个Section中提取出确实存在的文件Path"""
     for nsp, section in config.items():
-        if 'Default' in section:
-            yield section['Default']
+        if "Path" in section and Path(directory, section["Path"]).exists():
+            yield section["Path"]
+
+
+def parsedefault(config, directory):
+    """首先判断是否存在Default并且这个Default的值是一个Path，
+    同时这个Default所在这个Section不是以Profile开头，然后就能
+    拿到Default的配置文件
+    """
+    for nsp, section in config.items():
+        if (
+            "Default" in section
+            and not nsp.startswith("Profile")
+            and Path(directory, section["Default"]).exists()
+        ):
+            yield section["Default"]
+
 
 def get_profile_path():
     """获取火狐ini路径"""
-    firefox_dir = Path.home().joinpath(
-        '.mozilla', 'firefox')
-    return firefox_dir.joinpath('profiles.ini')
+    firefox_dir = Path.home().joinpath(".mozilla", "firefox")
+    return firefox_dir.joinpath("profiles.ini")
 
-def readcss():
-    scriptpath = Path(__file__).parent
-    csspath = scriptpath.joinpath('photon-australis',
-                                  'userChrome-dark.css')
-    with open(csspath, 'r') as f:
-        return f.readlines()
+
+def copyto(src, dst):
+    src_ = Path(src_path, src)
+    dst = Path(dst, src)
+    if not dst.exists():
+        logger.warning(f"复制{src_}到{dst}")
+        _copyto(src_, dst)
+    else:
+        logger.warning(f"跳过{src_}")
+
+
+def _copyto(src: Path, dst):
+    if src.is_dir():
+        shutil.copytree(src, dst)
+    else:
+        shutil.copy(src, dst)
+
 
 def main():
     profile = get_profile_path()
     logger.warning(f"配置文件路径位于{profile}")
-    csspath = parserini(profile).joinpath('userChrome.css')
-    logger.warning(f"css配置文件路径位于{csspath}")
-    with open(csspath, 'w') as f:
-        for line in readcss():
-            f.write(line)
+    profilepath = parserini(profile)
+    logger.warning(f"Profile文件夹路径位于{profilepath}")
+    for target in COPY_TARGETS:
+        copyto(target, profilepath)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("svg.context-properties.content.enabled", true);
